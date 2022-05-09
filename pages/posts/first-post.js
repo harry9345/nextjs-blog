9ae6bfb9d1953f26e2ef72a131c0b3bcc7c9dd23
Hossein movahed <hossein.movahed@monese.com>
2022-05-09 16:27:00 +0300

    adding css to next

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Page</title>
+        
             </Head>
             <h1>First Post</h1>
             <h2>
@@ -13,6 +15,6 @@ export default function FirstPost() {
                     <a>Back to home</a>
                 </Link>
             </h2>
-        </>
+        </Layout>
     );
 }
